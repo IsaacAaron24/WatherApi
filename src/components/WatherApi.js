4fd3568f719502e6a8a903de9e72bf9853b4dd0a
IsaacAaron24 <isaacaaron272@gmail.com>
2022-05-11 18:28:26 -0500

    primer commit

--- src/components/WatherApi.js
+++ src/components/WatherApi.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react'; 
 const WatherApi = () => {
 
     const color = ["#B3CFDD"]
